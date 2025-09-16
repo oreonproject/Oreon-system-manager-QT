a1a58d54ebc508228a64585315fb7eca4d9d0aa8
Ali moumen <moumenisbest@gmail.com>
2025-09-16 19:49:09 +0200

    Removed build artifacts

--- CMakeFiles/oreon-system-manager.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for oreon-system-manager.
-# This may be replaced when dependencies are built.
--- CMakeFiles/oreon-system-manager.dir/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/oreon-system-manager.dir/qrc_resources.cpp.o: \
- /home/ali/Oreon-system-manager/qrc_resources.cpp \
- /usr/include/stdc-predef.h
